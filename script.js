d0cee0c4651476501b1c149b91375e4aec7eecae
thaiminhbg2007-ai <thaiminhbg2007@gmail.com>
2026-07-19 07:15:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello ASEAN Passport!");
+const campaign = {
+
+    title: "ASEAN Vocab Passport",
+
+    subtitle: "Learn English through ASEAN Football",
+
+    buttonText: "Start Journey",
+
+    themeColor: "#006A4E"
+
+};
