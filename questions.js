c8224a8e26dad60139fa25fe3a41663d5a10ad10
thaiminhbg2007-ai <thaiminhbg2007@gmail.com>
2026-07-19 21:40:31 +0700

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,43 @@
+const questions = [
+
+{
+
+    question: "The striker ____ two goals yesterday.",
+
+    answers: [
+
+        "score",
+
+        "scored",
+
+        "scores",
+
+        "scoring"
+
+    ],
+
+    correct:1
+
+},
+
+{
+
+    question:"The referee showed a ____ card.",
+
+    answers:[
+
+        "red",
+
+        "goal",
+
+        "football",
+
+        "grass"
+
+    ],
+
+    correct:0
+
+}
+
+];
